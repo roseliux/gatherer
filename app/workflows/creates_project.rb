b52e3d3b2d576ba4cb1ffe7562a23f8f700fcdbc
roseliux <roxdurazo@gmail.com>
2018-09-12 11:04:16 -0700

    Add new test to CreatesProject.

--- app/workflows/creates_project.rb
+++ app/workflows/creates_project.rb
@@ -3,9 +3,26 @@ class CreatesProject
 
   def initialize(name: '', task_string: '')
     @name = name
+    @task_string = task_string
   end
 
   def build
     self.project = Project.new(name: name)
   end
+
+  def convert_string_to_tasks
+    tasks = []
+    task = convert_string_to_task(@task_string)
+    tasks << task if task
+    tasks
+  end
+
+  def convert_string_to_task(string)
+    title = string.split(':')[0]
+    return if title.nil?
+    size = string.split(':')[1].to_i
+    size = size.to_i <= 0 ? 1 : size
+    Task.new(title: title, size: size)
+  end
+
 end
--- spec/workflows/creates_project_spec.rb
+++ spec/workflows/creates_project_spec.rb
@@ -15,5 +15,42 @@ RSpec.describe CreatesProject do
       tasks = creator.convert_string_to_tasks
       expect(tasks).to be_empty
     end
+
+    it 'handles a single string' do
+      creator = CreatesProject.new(name: 'Project Runway', task_string: 'Start Things')
+      tasks = creator.convert_string_to_tasks
+      expect(tasks.size).to eq(1)
+      expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)
+    end
+
+    it 'handles a single string with size zero' do
+      creator = CreatesProject.new(name: 'Project Runway',
+                                   task_string: 'Start Things:0')
+      tasks = creator.convert_string_to_tasks
+      expect(tasks.size).to eq(1)
+      expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)
+    end
+
+    it 'handles a single string with malformed size' do
+      creator = CreatesProject.new(name: 'Project Runway',
+                                  task_string: 'Start Things:')
+      tasks = creator.convert_string_to_tasks
+      expect(tasks.size).to eq(1)
+      expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)
+    end
+
+    it 'handles a single string with negative size' do
+      creator = CreatesProject.new(name: 'Project Runway',
+                                   task_string: 'Start Things:-1')
+      tasks = creator.convert_string_to_tasks
+      expect(tasks.size).to eq(1)
+      expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)
+    end
+
+    it 'handles multiple task' do
+    end
+
+    it 'attaches tasks to the project' do
+    end
   end
 end
