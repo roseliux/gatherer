026085f38f8ff03b1ec0cb3d82ad325947010e97
roseliux <roxdurazo@gmail.com>
2018-09-16 20:10:01 -0700

    Making system end to end spec green.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,14 @@ class ProjectsController < ApplicationController
     @project = Project.new
   end
 
+  def index
+    @projects = Project.all
+  end
+
   def create
+    @workflow = CreatesProject.new(name: params[:project][:name],
+                                   task_string: params[:project][:tasks])
+    @workflow.create
+    redirect_to projects_path
   end
 end
--- spec/system/add_project_spec.rb
+++ spec/system/add_project_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'adding a project', type: :system do
   it 'allows a user to create a project with tasks' do
-    pending 'not implemented yet'
     visit new_project_path
     fill_in 'Name', with: 'Project Runway'
-    fill_in 'Tasks', with: 'Choose Fabric:3\nMake it Work:5'
+    fill_in 'Tasks', with: "Choose Fabric:3\nMake it Work:5"
     click_on('Create Project')
-    visit project_path
+    visit projects_path
     expect(page).to have_content('Project Runway')
     expect(page).to have_content(8)
   end
