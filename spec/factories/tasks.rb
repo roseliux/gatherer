0e8496152a7c5a504b2e5625ce490600d20d85cb
roseliux <roxdurazo@gmail.com>
2018-10-17 10:49:33 -0700

    Use trait in Task Factories.

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,35 @@
 FactoryBot.define do
   factory :task do
-    title { "Things to do" }
-    size { 1 }
-    completed_at { nil }
+    sequence(:title) { |n| "Task #{n}" }
+    size 3
+    completed_at nil
+
+    trait :small do
+      size 1
+    end
+
+    trait :large do
+      size 5
+    end
+
+    trait :soon do
+      due_date { 1.day.from_now }
+    end
+
+    trait :later do
+      due_date { 1.month.from_now }
+    end
+
+    trait :newly_complete do
+      completed_at { 1.day.ago }
+    end
+
+    trait :long_complete do
+      completed_at { 6.month.ago }
+    end
+
+    factory :trivial, class: Task, traits: %i[small later]
+    factory :panic, class: Task, traits: %i[large soon]
+
   end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -59,41 +59,42 @@ RSpec.describe Project do
   describe 'estimates' do
     let(:project) { FactoryBot.build_stubbed(:project,
       tasks: [newly_done, old_done, small_not_done, large_not_done]) }
-    let(:newly_done) { FactoryBot.build_stubbed(:task,
-      size: 3, completed_at: 1.day.ago) }
-    let(:old_done) { FactoryBot.build_stubbed(:task,
-      size: 2, completed_at: 6.months.ago) }
-    let(:small_not_done) { FactoryBot.build_stubbed(:task, size: 1) }
-    let(:large_not_done) { FactoryBot.build_stubbed(:task, size: 4) }
+    let(:newly_done) { FactoryBot.build_stubbed(:task, :newly_complete) }
+    let(:old_done) { FactoryBot.build_stubbed(:task, :long_complete, :small) }
+    let(:small_not_done) { FactoryBot.build_stubbed(:task, :small) }
+    let(:large_not_done) { FactoryBot.build_stubbed(:task, :large) }
 
-    it 'can calculate total size' do
+  it "can calculate total size" do
       expect(project).to be_of_size(10)
+      expect(project).not_to be_of_size(5)
     end
 
-    it 'can calculate remaining size' do
-      expect(project).to be_of_size(5).for_incomplete_tasks_only
+    it "can calculate remaining size" do
+      expect(project).to be_of_size(6).for_incomplete_tasks_only
+    end
+    #
+
+    it "knows its velocity" do
+      expect(project.completed_velocity).to eq(3)
     end
 
-    it 'knows its velocity' do
-      expect(project.completed_velocity).to eq 3
-    end
-
-    it 'knows its rate' do
+    it "knows its rate" do
       expect(project.current_rate).to eq(1.0 / 7)
     end
 
-    it 'knows its projected days remaining' do
-      expect(project.projected_days_remaining).to eq(35)
+    it "knows its projected time remaining" do
+      expect(project.projected_days_remaining).to eq(42)
     end
 
-    it 'knows if it is not in schedule' do
+    it "knows if it is not on schedule" do
       project.due_date = 1.week.from_now
       expect(project).not_to be_on_schedule
     end
 
-    it 'knows if it is on schedule' do
+    it "knows if it is on schedule" do
       project.due_date = 6.months.from_now
       expect(project).to be_on_schedule
     end
   end
+
 end
